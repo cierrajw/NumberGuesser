90a30df0f410732be5b64ade730d6bcd41ac55c3
Cierra Washington <cierrajwashi@gmail.com>
2018-07-03 11:09:16 -0600

    Changing visual layout, spacing/margin

--- app.js
+++ app.js
@@ -12,14 +12,15 @@
 
 // }
 
-	var button = document.getElementById("submitButton");
+	var button = document.querySelector('#submitButton');
 
-
-button.addEventListener("click", function () {
+	button.addEventListener("click", function () {
 
 	var theNumber = Math.floor((Math.random()* (101 - 1) + 1));
 
-	var userGuess = document.getElementById("userGuess");
+	console.log("The random number: " + theNumber);
+
+	var userGuess = document.querySelector('#userGuess');
 
 	var answer = parseInt(userGuess.value); 
 
@@ -27,84 +28,27 @@ button.addEventListener("click", function () {
 
 	if((answer >= 1) && (answer <= 100)){
 
-		document.getElementById("lastGuess").innerHTML = "Your last guess was";
+		document.querySelector('#lastGuess').innerHTML = "Your last guess was";
 
-		document.getElementById("output").innerHTML = answer;
+		document.querySelector('#output').innerHTML = answer;
 
 	if(answer > theNumber){
 		console.log("That is too high");
-		document.getElementById("result").innerHTML = "That is too high";
+		document.querySelector('#result').innerHTML = "That is too high";
 	}
 	else if (answer < theNumber){
 		var toolow = "That is too low";
 		console.log("That is too low");
-		document.getElementById("result").innerHTML = "That is too low";
+		document.querySelector('#result').innerHTML = "That is too low";
 	}
 	else{
 		console.log("BOOM goes the dynamite!!!");
-		document.getElementById("result").innerHTML = "BOOM goes the dynamite!!!";
+		document.querySelector('#result').innerHTML = "BOOM goes the dynamite!!!";
 		}
 	}
 	else{
 		console.log("Please enter a number between 1 and 100");
-		document.getElementById("result").innerHTML = "Please enter a number between 1 and 100";
+		document.querySelector('#result').innerHTML = "Please enter a number between 1 and 100";
 	}
 	console.log(answer);
 });
-
-// function resetValues(){
-
-// 	reset = document.getElementById("resetButton").innerHTML;
-
-// 	userGuess.reset();
-// 	document.getElementById("lastGuess").reset();
-// 	document.getElementById("output").reset();
-// }
-
-// reset.addEventListener("click", resetValues){
-
-// }
-
-
-//-------------------------------- NOTES:---------------------------
-
-// BAD CODE: var userGuess = parseInt(document.getElementById("userGuess")).value;
-
-// can use document.querySelector("h1") can use h1 (selector) or #id or .class, etc.
-
-	//can change HTML input to make sure it's a number instead
-	// else if(theNumber == isNaN(userGuess)){
-	
-	// 	alert("Please enter an actual number");
-	// 	return "Please enter an actual number";
-	// }
-
-// Use switch statement? 
-
-	// switch(userGuess){
-
-	// case userGuess > theNumber:
-	// alert("That is too high");
-	// return "That is too high";
-	// break;
-
-	// case userGuess < theNumber:
-	// alert("That is too low");
-	// return "That is too low";
-	// break;
-
-	// case ((theNumber < 1) || (theNumber > 100));
-	// alert("Enter a number between 1 and 100");
-	// return "Enter a number between 1 and 100";
-
-	// case userGuess == isNaN(userGuess):
-	// return "You must enter a number!";
-	// break;
-
-	// //Would this be the default instead?
-	// case userGuess == theNumber:
-	// alert("Boom!!!");
-	// return "BOOM!!!";
-	// break;
-	// }
-
